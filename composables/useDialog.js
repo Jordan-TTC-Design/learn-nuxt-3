bc00022a6feb10151012cc3ffb98773c4a216ff0
JordanTseng <jordan.ttc.design@gmail.com>
2023-06-11 22:08:50 +0800

    feat: 新增自訂義 dialog

--- composables/useDialog.js
+++ composables/useDialog.js
@@ -1,1 +1,41 @@
-
+export const dialogOption = reactive({
+  title: '',
+  content: '',
+  confirm: {
+    btnName: '',
+    onComplete: () => {},
+  },
+  cancel: {
+    btnName: '',
+    onComplete: () => {},
+  },
+});
+const dialogDom = ref(null);
+export const useDialog = () => {
+  const close = type => {
+    if (dialogOption[type].onComplete) {
+      dialogOption[type].onComplete();
+    }
+    dialogDom.value.close();
+  };
+  const open = option => {
+    if (option) {
+      dialogOption.title = option.title;
+      dialogOption.content = option.content;
+      dialogOption.confirm.btnName = option.confirm.btnName;
+      dialogOption.confirm.onComplete = option.confirm.onComplete;
+      dialogOption.cancel.btnName = option.cancel.btnName;
+      dialogOption.cancel.onComplete = option.cancel.onComplete;
+    }
+    console.log('open');
+    dialogDom.value.showModal();
+  };
+  onMounted(() => {
+    dialogDom.value = document.querySelector('#dialog');
+    console.log('mounted');
+  });
+  return {
+    close,
+    open,
+  };
+};
